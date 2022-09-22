972ef577b09dd56420b87f6e92690e54b37a14ae
Nicole Tough <nicole.tough@bgcaustin.org>
2022-09-22 07:14:24 -0500

    Adding retirement_info and queries

--- schema.sql
+++ schema.sql
@@ -39,7 +39,7 @@ CREATE TABLE titles(
 	emp_no int NOT NULL,
 	title varchar NOT NULL,
 	from_date date NOT NULL,
+	to_date date NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
-
